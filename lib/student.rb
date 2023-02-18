2ed1587b475565cbd0e8b3564deb60300c0dd721
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-18 10:10:28 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string_of_knowlege)
+        @knowledge << string_of_knowlege
+    end
+    # def knowledge
+    #     @knowledge
+    # end
 
 end
